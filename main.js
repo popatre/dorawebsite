a22d9c9ba7a3b416884d9500f62969460100a4f7
popatre <76402245+popatre@users.noreply.github.com>
2021-10-06 20:53:58 +0100

    adds animations to the hero title

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 
+// turns violet on page load
+const violet = document.querySelector('.violet')
+const dora = document.querySelector('.dorav');
+const colquhoun = document.querySelector('.colquhoun');
+
+// hero fade in
+window.onload = doraC();
+function doraC(){
+    
+    
+   
+};
+
+window.onload = setTimeout(doraC, 1000);
+function doraC(){
+    // $(".dorav").fadeIn(1000);
+    $(".dorav").animate({opacity: 1}, 1000);
+}
+window.onload = setTimeout(colq, 1000);
+function colq(){
+    // $(".colquhoun").fadeIn(1000);
+    $(".colquhoun").animate({opacity: 1}, 1000);
+}
+
+window.onload = setTimeout(vio, 3500);
+function vio(){
+    // $(".violet").fadeIn(3000);
+    $(".violet").animate({opacity: 1}, 1000);
+}
+
+
+window.onload = setTimeout(violetTurner, 7000);
+function violetTurner(){
+violet.classList.add('turnviolet');
+}
+
+
+
+//////////////////////////////////////////
+/* sub title fade ins */
+window.onload = setTimeout(subFader, 8000);
+function subFader(){
+$('.hero__sub').animate({opacity: 1}, 1000).animate({'opacity': 0}, 2000, function () {
+    $(this).text('Devisor');
+}).animate({'opacity': 1}, 1000).animate({'opacity': 0}, 2000, function () {
+    $(this).text('Facilitator');
+}).animate({'opacity': 1}, 1000).animate({'opacity': 0}, 2000, function () {
+    $(this).text('Performer, Devisor, Facilitator');
+}).animate({'opacity': 1}, 1000);
+};
+/************************************ */
