4ca86dcfe9269b0e7ab99a070f1494e00e6785da
popatre <76402245+popatre@users.noreply.github.com>
2023-08-30 18:04:42 +0100

    add slick slider

---
+++ slick.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $(".slick-slider").slick({
+        lazyLoad: "ondemand",
+        adaptiveHeight: true,
+        rows: 1,
+        swipeToSlide: true,
+        arrows: true,
+        mobileFirst: true,
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
+});
